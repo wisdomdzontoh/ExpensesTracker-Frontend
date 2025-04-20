4b872fbab28de1d1078beff5568c87b6179aa217
wisdomdzontoh <wisdomdzontoh@gmail.com>
2025-04-20 13:04:18 +0000

    get base url from env

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,8 +1,10 @@
-import axios from 'axios'
+// lib/api.ts (or wherever you configure axios)
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api', // Update as per your Django backend
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api",
   withCredentials: true,
-})
+});
 
-export default api
+export default api;
+
